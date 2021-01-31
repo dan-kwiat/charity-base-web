6e08ee3161dd007035df8d8a2b98bd8613a7db7f
dan-kwiat <dan.kwiat@gmail.com>
2021-01-31 21:54:27 +0000

    chore(): remove graphiql

--- src/components/api-portal/getting-started/index.js
+++ src/components/api-portal/getting-started/index.js
@@ -1,11 +1,9 @@
 import React from "react"
 import styled from "styled-components"
-import { Link } from "react-router-dom"
 import { CenteredContent, ResponsiveScroll } from "../../general/Layout"
 import { Icon, Typography } from "antd"
-import Example1 from "./example-1"
 
-const { Paragraph, Title } = Typography
+const { Title } = Typography
 
 const Section = styled.div`
   margin-bottom: 5em;
@@ -24,103 +22,103 @@ const Section = styled.div`
   }
 `
 
-const SectionText = styled.div`
-  font-size: 1.3em;
-  margin: 1.5em auto;
-  max-width: 730px;
-`
+// const SectionText = styled.div`
+//   font-size: 1.3em;
+//   margin: 1.5em auto;
+//   max-width: 730px;
+// `
 
-const GettingStarted = () => {
-  return (
-    <ResponsiveScroll>
-      <CenteredContent>
-        <Section>
-          <Title>
-            <Icon type="api" style={{ marginRight: "0.5em" }} />
-            Getting Started with the API
-          </Title>
-        </Section>
-        <Section>
-          <Title level={3}>What's an API?</Title>
-          <SectionText>
-            <Paragraph>
-              An API is a generic term for the tool which one piece of software
-              uses to communicate with another piece of software. Don't worry
-              about what it stands for.
-            </Paragraph>
-            <Paragraph>
-              Put simply, the CharityBase API{" "}
-              <strong>
-                <i>allows anyone to plug into our database</i>
-              </strong>
-              . A user of the API can send a request for specific information
-              and the API will respond almost immediately with the relevant
-              structured data.
-            </Paragraph>
-            <Paragraph>
-              The CharityBase API enables organisations to build charitable
-              digital tools without needing to do the heavy lifting of
-              collecting, cleaning, storing, aggregating & serving data.
-            </Paragraph>
-          </SectionText>
-        </Section>
-        <Section>
-          <Title level={3}>CharityBase Elements Overview</Title>
-          <SectionText>
-            <Paragraph>
-              CharityBase Elements is a set of prebuilt UI components, like
-              inputs and maps, which utilise the API for common use cases.
-              Elements are completely customisable and you can style Elements to
-              match the look and feel of your site. They're coming soon...
-            </Paragraph>
-          </SectionText>
-        </Section>
-        <Section>
-          <Title level={3}>Using the API directly</Title>
-          <SectionText>
-            <Paragraph>If Elements do not serve your needs...</Paragraph>
-            <Paragraph>
-              CharityBase is a{" "}
-              <a
-                href="https://graphql.org"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                GraphQL
-              </a>{" "}
-              API which gives you the power to ask for exactly the data you
-              need. There's a single endpoint{" "}
-              <code>https://charitybase.uk/api/graphql</code> where you can send
-              either a <code>GET</code> or a <code>POST</code> request. The
-              request must contain a <code>query</code> parameter in the query
-              string or optionally in the JSON body (if using <code>POST</code>
-              ). You must also supply an <code>Authorization</code> header of
-              the form <code>Apikey YOUR_API_KEY</code>.
-            </Paragraph>
-            <Paragraph>
-              Here's a basic example which counts all charities registered with
-              the Charity Commission. Remember to replace{" "}
-              <code>YOUR_API_KEY</code> with{" "}
-              <Link to="/api-portal/keys">your actual key</Link>.
-            </Paragraph>
-            <Paragraph>
-              <Example1 />
-            </Paragraph>
-            <Paragraph>
-              To experiment with the query language and see what fields are
-              available,{" "}
-              <Link to="/api-portal/playground">visit the playground</Link>. The
-              contents of the left panel makes up the query string to send in
-              your request, as shown in the examples above. Click on the "Docs"
-              button in the top right corner of the playground for an
-              interactive schema.
-            </Paragraph>
-          </SectionText>
-        </Section>
-      </CenteredContent>
-    </ResponsiveScroll>
-  )
-}
+// const GettingStarted = () => {
+//   return (
+//     <ResponsiveScroll>
+//       <CenteredContent>
+//         <Section>
+//           <Title>
+//             <Icon type="api" style={{ marginRight: "0.5em" }} />
+//             Getting Started with the API
+//           </Title>
+//         </Section>
+//         <Section>
+//           <Title level={3}>What's an API?</Title>
+//           <SectionText>
+//             <Paragraph>
+//               An API is a generic term for the tool which one piece of software
+//               uses to communicate with another piece of software. Don't worry
+//               about what it stands for.
+//             </Paragraph>
+//             <Paragraph>
+//               Put simply, the CharityBase API{" "}
+//               <strong>
+//                 <i>allows anyone to plug into our database</i>
+//               </strong>
+//               . A user of the API can send a request for specific information
+//               and the API will respond almost immediately with the relevant
+//               structured data.
+//             </Paragraph>
+//             <Paragraph>
+//               The CharityBase API enables organisations to build charitable
+//               digital tools without needing to do the heavy lifting of
+//               collecting, cleaning, storing, aggregating & serving data.
+//             </Paragraph>
+//           </SectionText>
+//         </Section>
+//         <Section>
+//           <Title level={3}>CharityBase Elements Overview</Title>
+//           <SectionText>
+//             <Paragraph>
+//               CharityBase Elements is a set of prebuilt UI components, like
+//               inputs and maps, which utilise the API for common use cases.
+//               Elements are completely customisable and you can style Elements to
+//               match the look and feel of your site. They're coming soon...
+//             </Paragraph>
+//           </SectionText>
+//         </Section>
+//         <Section>
+//           <Title level={3}>Using the API directly</Title>
+//           <SectionText>
+//             <Paragraph>If Elements do not serve your needs...</Paragraph>
+//             <Paragraph>
+//               CharityBase is a{" "}
+//               <a
+//                 href="https://graphql.org"
+//                 target="_blank"
+//                 rel="noopener noreferrer"
+//               >
+//                 GraphQL
+//               </a>{" "}
+//               API which gives you the power to ask for exactly the data you
+//               need. There's a single endpoint{" "}
+//               <code>https://charitybase.uk/api/graphql</code> where you can send
+//               either a <code>GET</code> or a <code>POST</code> request. The
+//               request must contain a <code>query</code> parameter in the query
+//               string or optionally in the JSON body (if using <code>POST</code>
+//               ). You must also supply an <code>Authorization</code> header of
+//               the form <code>Apikey YOUR_API_KEY</code>.
+//             </Paragraph>
+//             <Paragraph>
+//               Here's a basic example which counts all charities registered with
+//               the Charity Commission. Remember to replace{" "}
+//               <code>YOUR_API_KEY</code> with{" "}
+//               <Link to="/api-portal/keys">your actual key</Link>.
+//             </Paragraph>
+//             <Paragraph>
+//               <Example1 />
+//             </Paragraph>
+//             <Paragraph>
+//               To experiment with the query language and see what fields are
+//               available,{" "}
+//               <Link to="/api-portal/playground">visit the playground</Link>. The
+//               contents of the left panel makes up the query string to send in
+//               your request, as shown in the examples above. Click on the "Docs"
+//               button in the top right corner of the playground for an
+//               interactive schema.
+//             </Paragraph>
+//           </SectionText>
+//         </Section>
+//       </CenteredContent>
+//     </ResponsiveScroll>
+//   )
+// }
 
 // <Section>
 //   <Title level={3}>
--- src/components/api-portal/playground/index.js
+++ src/components/api-portal/playground/index.js
@@ -1,14 +1,4 @@
-import React, { Component } from "react"
-import PropTypes from "prop-types"
-import { parse, print } from "graphql"
-import GraphiQL from "graphiql"
-import defaultQuery from "./defaultQuery"
-import ApiKeyModal from "./ApiKeyModal"
-import { charityBaseGqlApiUri } from "../../../lib/constants"
-import auth from "../../../lib/auth"
-import "graphiql/graphiql.css"
-import Button from "../../general/Button"
-
+import React from "react"
 import styled from "styled-components"
 import { CenteredContent, ResponsiveScroll } from "../../general/Layout"
 import { Icon, Typography } from "antd"
@@ -32,88 +22,88 @@ const Section = styled.div`
   }
 `
 
-const getGraphQLFetcher = (apiKey) => (graphQLParams) => {
-  const { user } = auth
-  return fetch(charityBaseGqlApiUri, {
-    method: "post",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: apiKey ? `Apikey ${apiKey}` : undefined,
-      UserId: user ? user.sub : undefined,
-    },
-    body: JSON.stringify(graphQLParams),
-  }).then((response) => response.json())
-}
+// const getGraphQLFetcher = (apiKey) => (graphQLParams) => {
+//   const { user } = auth
+//   return fetch(charityBaseGqlApiUri, {
+//     method: "post",
+//     headers: {
+//       "Content-Type": "application/json",
+//       Authorization: apiKey ? `Apikey ${apiKey}` : undefined,
+//       UserId: user ? user.sub : undefined,
+//     },
+//     body: JSON.stringify(graphQLParams),
+//   }).then((response) => response.json())
+// }
 
-class Playground extends Component {
-  state = {
-    isApiKeyModalOpen: this.props.apiKey ? false : true,
-  }
-  handlePrettifyQuery = (event) => {
-    const editor = this.graphiqlComp.getQueryEditor()
-    editor.setValue(print(parse(editor.getValue())))
-  }
-  handleToggleHistory = () => {
-    this.graphiqlComp.setState((s) => ({
-      historyPaneOpen: !s.historyPaneOpen,
-    }))
-  }
-  render() {
-    const { apiKey, setApiKey } = this.props
-    const { isApiKeyModalOpen } = this.state
-    return (
-      <div className="api-explorer-container">
-        <GraphiQL
-          ref={(c) => {
-            this.graphiqlComp = c
-          }}
-          fetcher={getGraphQLFetcher(apiKey)}
-          defaultQuery={defaultQuery}
-        >
-          <GraphiQL.Logo>GraphiQL</GraphiQL.Logo>
-          <GraphiQL.Button label="test button" />
-          <GraphiQL.Toolbar>
-            <Button
-              onClick={this.handlePrettifyQuery}
-              title="Prettify Query (Shift-Ctrl-P)"
-              style={{ margin: "0 0.5em" }}
-            >
-              Prettify
-            </Button>
-            <Button
-              onClick={this.handleToggleHistory}
-              title="Show History"
-              style={{ margin: "0 0.5em" }}
-            >
-              History
-            </Button>
-            <Button
-              onClick={() => this.setState({ isApiKeyModalOpen: true })}
-              title="Set Authorization Header"
-              solid={apiKey || isApiKeyModalOpen ? false : true}
-              style={{ margin: "0 0.5em" }}
-            >
-              Set Auth Header
-            </Button>
-          </GraphiQL.Toolbar>
-        </GraphiQL>
-        <ApiKeyModal
-          currentKey={apiKey}
-          isOpen={isApiKeyModalOpen}
-          onChange={(apiKey) => {
-            setApiKey(apiKey)
-            this.setState({ isApiKeyModalOpen: false })
-          }}
-          onClose={() => this.setState({ isApiKeyModalOpen: false })}
-        />
-      </div>
-    )
-  }
-}
-Playground.propTypes = {
-  apiKey: PropTypes.string,
-  setApiKey: PropTypes.func.isRequired,
-}
+// class Playground extends Component {
+//   state = {
+//     isApiKeyModalOpen: this.props.apiKey ? false : true,
+//   }
+//   handlePrettifyQuery = (event) => {
+//     const editor = this.graphiqlComp.getQueryEditor()
+//     editor.setValue(print(parse(editor.getValue())))
+//   }
+//   handleToggleHistory = () => {
+//     this.graphiqlComp.setState((s) => ({
+//       historyPaneOpen: !s.historyPaneOpen,
+//     }))
+//   }
+//   render() {
+//     const { apiKey, setApiKey } = this.props
+//     const { isApiKeyModalOpen } = this.state
+//     return (
+//       <div className="api-explorer-container">
+//         <GraphiQL
+//           ref={(c) => {
+//             this.graphiqlComp = c
+//           }}
+//           fetcher={getGraphQLFetcher(apiKey)}
+//           defaultQuery={defaultQuery}
+//         >
+//           <GraphiQL.Logo>GraphiQL</GraphiQL.Logo>
+//           <GraphiQL.Button label="test button" />
+//           <GraphiQL.Toolbar>
+//             <Button
+//               onClick={this.handlePrettifyQuery}
+//               title="Prettify Query (Shift-Ctrl-P)"
+//               style={{ margin: "0 0.5em" }}
+//             >
+//               Prettify
+//             </Button>
+//             <Button
+//               onClick={this.handleToggleHistory}
+//               title="Show History"
+//               style={{ margin: "0 0.5em" }}
+//             >
+//               History
+//             </Button>
+//             <Button
+//               onClick={() => this.setState({ isApiKeyModalOpen: true })}
+//               title="Set Authorization Header"
+//               solid={apiKey || isApiKeyModalOpen ? false : true}
+//               style={{ margin: "0 0.5em" }}
+//             >
+//               Set Auth Header
+//             </Button>
+//           </GraphiQL.Toolbar>
+//         </GraphiQL>
+//         <ApiKeyModal
+//           currentKey={apiKey}
+//           isOpen={isApiKeyModalOpen}
+//           onChange={(apiKey) => {
+//             setApiKey(apiKey)
+//             this.setState({ isApiKeyModalOpen: false })
+//           }}
+//           onClose={() => this.setState({ isApiKeyModalOpen: false })}
+//         />
+//       </div>
+//     )
+//   }
+// }
+// Playground.propTypes = {
+//   apiKey: PropTypes.string,
+//   setApiKey: PropTypes.func.isRequired,
+// }
 
 // export default Playground
 
