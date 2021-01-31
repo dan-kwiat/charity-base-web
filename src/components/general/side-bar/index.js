a9e0eb23f3b2b5e12301859135f78d3dceab83c6
dan-kwiat <dan.kwiat@gmail.com>
2021-01-31 22:06:05 +0000

    feat(layout): make logo link to charitybase.uk

--- src/components/general/side-bar/index.js
+++ src/components/general/side-bar/index.js
@@ -1,12 +1,11 @@
-import React, { Fragment, useState } from 'react'
-import { Route } from 'react-router-dom'
-import styled from 'styled-components'
-import { Layout, Divider } from 'antd'
-import { HomeLink } from '../../general/Layout'
-import LogInOrOut from '../../general/LogInOrOut'
-import NavMenu from './NavMenu'
-import ListenToProp from './ListenToProp'
-import { ReactComponent as TwitterIcon } from './twitter.svg'
+import React, { Fragment, useState } from "react"
+import { Route } from "react-router-dom"
+import styled from "styled-components"
+import { Layout, Divider } from "antd"
+import LogInOrOut from "../../general/LogInOrOut"
+import NavMenu from "./NavMenu"
+import ListenToProp from "./ListenToProp"
+import { ReactComponent as TwitterIcon } from "./twitter.svg"
 
 const SIDER_WIDTH = 240
 
@@ -16,58 +15,65 @@ const Sider = styled(Layout.Sider)`
   max-height: 100vh !important;
   position: fixed !important;
   left: 0 !important;
-  color: rgba(255,255,255,0.8) !important;
+  color: rgba(255, 255, 255, 0.8) !important;
   z-index: 2 !important;
+`
+
+const LargeAnchorTag = styled.a`
+  color: #ec407a;
+  font-size: 2em;
+  letter-spacing: 0.13em;
+  font-weight: 300;
+  display: block;
+  margin-bottom: 1em;
+  :hover {
+    color: #d81b60;
+  }
 `
 
 const SideBar = () => {
   const [chcIds, setChcIds] = useState([])
   return (
     <Fragment>
-      <Route path='/chc/:id' render={({ match }) => (
-        <ListenToProp
-          stringProp={match.params.id}
-          onChange={id => {
-            if (!id) return
-            if (chcIds.indexOf(id) > -1) return
-            setChcIds([...chcIds, id])
-          }}
-        />
-      )} />
-      <Sider
-        breakpoint='lg'
-        collapsedWidth={0}
-        width={SIDER_WIDTH}
-      >
+      <Route
+        path="/chc/:id"
+        render={({ match }) => (
+          <ListenToProp
+            stringProp={match.params.id}
+            onChange={(id) => {
+              if (!id) return
+              if (chcIds.indexOf(id) > -1) return
+              setChcIds([...chcIds, id])
+            }}
+          />
+        )}
+      />
+      <Sider breakpoint="lg" collapsedWidth={0} width={SIDER_WIDTH}>
         <div
           style={{
-            padding: '1em',
-            textAlign: 'center',
+            padding: "1em",
+            textAlign: "center",
           }}
         >
-          <HomeLink to='/'>CharityBase</HomeLink>
+          <LargeAnchorTag href="https://charitybase.uk">
+            CharityBase
+          </LargeAnchorTag>
           <LogInOrOut
-            renderLoggedInText={name => <p>Logged in as {name}</p>}
-            renderLoggedOutText={name => null}
+            renderLoggedInText={(name) => <p>Logged in as {name}</p>}
+            renderLoggedOutText={(name) => null}
           />
         </div>
-        <NavMenu
-          chcIds={chcIds}
-        />
+        <NavMenu chcIds={chcIds} />
         <Divider />
         <div
           style={{
-            padding: '0 1em',
-            marginBottom: '2em',
-            textAlign: 'center',
+            padding: "0 1em",
+            marginBottom: "2em",
+            textAlign: "center",
           }}
         >
-          <a href='https://twitter.com/charitybase_uk' >
-            <TwitterIcon
-              width='2em'
-              height='2em'
-              fill='rgba(255,255,255,.8)'
-            />
+          <a href="https://twitter.com/charitybase_uk">
+            <TwitterIcon width="2em" height="2em" fill="rgba(255,255,255,.8)" />
           </a>
         </div>
       </Sider>
